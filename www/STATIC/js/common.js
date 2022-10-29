ba7e2d87040ccab6cfc73d0bc1f934ea86fdf6ac
test0926 <99738531+test0926@users.noreply.github.com>
2022-10-29 20:41:24 +0900

    Add files via upload

--- www/STATIC/js/common.js
+++ www/STATIC/js/common.js
@@ -285,6 +285,12 @@ $(window).on("scroll", function(){
 	}
 });
 
+function setScreenSize() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+setScreenSize();
 
 $( function(){
 
